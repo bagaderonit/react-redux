833f72e2dc69f0cb822dbcdadc91ac662acb94c5
unknown <bagaderonit@gmail.com>
2021-06-05 00:07:03 -0700

    reducer ation added as default

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import {} from 'react-redux'
+import { Provider } from 'react-redux';
+import {store} from './redux/store'
 import './App.css';
 import Cakes from './Component/Cakes';
 
 function App() {
   return (
-    <div className="App">
-      <Cakes/>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <Cakes />
+      </div>
+    </Provider>
   );
 }
 
