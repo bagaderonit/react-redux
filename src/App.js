2c3de925b3787a316d7c22c401edd94f68a5dc8e
unknown <bagaderonit@gmail.com>
2021-06-04 23:26:26 -0700

    hi

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import {} from 'react-redux'
+import './App.css';
+import Cakes from './Component/Cakes';
+
+function App() {
+  return (
+    <div className="App">
+      <Cakes/>
+    </div>
+  );
+}
+
+export default App;
